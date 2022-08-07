d139916e521debc6accd2ed8e3da2dcb60a03b5c
Mikhail Falaleev <falaleev_2018@inbox.ru>
2022-08-07 11:25:13 +0300

    solution

--- int.py
+++ int.py
@@ -1,9 +1,14 @@
+from sys import getsizeof
+
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
+def size():
+    print(getsizeof((3 ** 9000) / (1024 * 1024)))
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
+    size()
